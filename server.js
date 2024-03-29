7aaddc934dd7f87e46fd6aa921748010ec826541
Sakar <sk4sakar@gmail.com>
2024-03-29 12:34:17 +0530

    Removed changes

--- server.js
+++ server.js
@@ -17,7 +17,7 @@ connectDB();
 // middlewares
 app.use(cors())
 app.use(express.json())
-app.use(express.static(path.join(__dirname,"./client/build")))
+// app.use(express.static(path.join(__dirname,"./client/build")))
 
 const port = process.env.PORT || 8080
 
@@ -27,9 +27,9 @@ app.use('/api/v1/auth',auth)
 app.use('/api/v1/category',categoryRoute)
 app.use('/api/v1/product',productRoute)
 
-app.use("*",function(req,res){
-    res.sendFile(path.join(__dirname,"./client/build/index.html"))
-})
+// app.use("*",function(req,res){
+//     res.sendFile(path.join(__dirname,"./client/build/index.html"))
+// })
 
 app.listen(port,()=>{
     console.log(`Listening on PORT ${port}`)
